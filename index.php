4b7acb9ac60382915693be88eff021321064915a
Guel de Almeida <guelgaietaalmida@gmail.com>
2020-09-11 04:31:41 +0000

    add favicon

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
+    <link rel="shortcut icon" href="favicon.ico">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
