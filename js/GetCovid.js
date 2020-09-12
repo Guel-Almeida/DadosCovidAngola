f34d340dd8cd98b9a2896805478dfa39f623f609
Guel de Almeida <guelgaietaalmida@gmail.com>
2020-09-12 00:02:38 +0000

    resolvendo problema dos merges

--- js/GetCovid.js
+++ js/GetCovid.js
@@ -2,34 +2,7 @@ window.onload = ()=>{
   
     const TotalPeople = 29572984
   
-    GetCovid()
-
-
-<<<<<<< HEAD
-=======
-    function GetCovid(){
-         fetch(url, {
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    
-    }).then((response) => response.json())
-        .then((res) => {
-            ValorActive.innerHTML= res.data.active
-            ValorDeath.innerHTML = res.data.deaths
-            ValorRecovered.innerHTML= res.data.recovered
-            dadosActivos.innerHTML = ((Number(res.data.active)*100)/Number(TotalPeople)).toFixed(5)+" %"
-            dadosPeople.innerHTML = TotalPeople
-            dadosDeath.innerHTML = ((Number(res.data.deaths)*100)/Number(TotalPeople)).toFixed(5)+" %"
-            dadosRecovered.innerHTML = ((Number(res.data.recovered)*100)/Number(TotalPeople)).toFixed(5)+" %"
-            
-        }).catch((error) => {
-            console.error(error)
-    });
-    }
->>>>>>> developement
-   
+    GetCovid()   
 
 }
 
