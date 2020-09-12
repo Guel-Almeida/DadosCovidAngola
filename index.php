1862cabb8d62df6678489e4d0975d52c6ae72ba5
Guel de Almeida <guelgaietaalmida@gmail.com>
2020-09-12 00:08:45 +0000

    resolvendo tudo

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 <header><h1>Covid Angola</h1></header>
 <div class="container-fluid">
 
-<<<<<<< HEAD
 <main>
 <header>Covid Angola</header>
 
@@ -49,55 +48,6 @@
      <p class="me">Desenvolvido por Guel de Almeida</p>
 </footer>
            
-=======
-<div class="row-fluid">
-<div class="geral active col-sm-12 col-md-4 col-offset-md-6">
-        <header class="headerActive"><h3>Activo</h3></header>
-        <div class="dados">
-            <p class="ValorActive"></p>
-        </div>
-       
-</div>
-
-<div class="geral death col-sm-12 col-md-4 col-offset-md-6">
-    <header class="headerDeath"><h3>Mortos</h3></header>
-    <div class="dados"><p class="ValorDeath"></p></div>
-</div>
-
-<div class="geral recovered col-sm-12 col-md-4 col-offset-md-6">
-   
-    <header class="headerRecovered"><h3>Recuperados</h3></header>
-    <div class="dados"> <p class="ValorRecovered"></p></div>
-   
-</div>
-</div>
-</div>
-<div class="row-fluid">
-<footer class="footer">
-<table class="table .table-dark">
-  <thead>
-    <tr>
-      <th >População</th>
-      <th >Activos</th>
-      <th > Mortos</th>
-      <th >Recuperados</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td id="dadosPeople"></td>
-      <td id="dadosActivos"></td>
-      <td id="dadosDeath"></td>
-      <td id="dadosRecovered"></td>
-    </tr>
-  </tbody>
-    
-</table>
-     Desenvolvido por Miguel Almeida
-</footer>
-</div>
-
->>>>>>> developement
 
 
 
